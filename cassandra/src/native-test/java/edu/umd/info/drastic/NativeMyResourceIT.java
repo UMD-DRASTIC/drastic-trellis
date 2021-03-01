1a7418ad774ff0b28293d53a9570a6d71bb0b0fc
Gregory N Jansen <gregory.jansen@gmail.com>
2021-03-01 18:15:26 -0500

    unworking attempt to split components from cassandra runtime build

---
+++ cassandra/src/native-test/java/edu/umd/info/drastic/NativeMyResourceIT.java
@@ -0,0 +1,9 @@
+package edu.umd.info.drastic;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMyResourceIT extends DigestHeaderTest {
+
+    // Execute the same tests but in native mode.
+}
